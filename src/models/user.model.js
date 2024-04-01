14e9ef8ce036c96d1a475c44f700a3ceb7a1d417
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2024-04-01 12:44:13 +0000

    Added User & Video Model - Implemented bcrypt & JWT

---
+++ src/models/user.model.js
@@ -0,0 +1,84 @@
+import mongoose from "mongoose";
+import { jwt } from "jsonwebtoken";
+import bcrypt from "bcrypt";
+
+const userSchema = new mongoose.Schema({
+    username: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true,
+        index: true
+    },
+    password: {
+        type: String,
+        unique: true,
+        required: [true, "Password is required"]
+    },
+    email: {
+        type: String,
+        unique: true,
+        required: true,
+        lowercase: true,
+        trim: true,
+    },
+    fullName: {
+        type: String,
+        required: true,
+        index: true,
+        trim: true
+    },
+    avatar: {
+        type: String,
+        required: true
+    },
+    coverImage: {
+        type: String,
+    },
+    watchHistory: [
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "Video"
+        }
+    ],
+    refreshToken: {
+
+    }
+
+}, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+    if (!this.isModified("password")) return next()
+    this.password = bcrypt.hash(this.password, 10)
+    next()
+})
+
+userSchema.methods.isPasswordCorrect = async function (password) {
+    return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function () {
+    return jwt.sign({
+        _id: this._id,
+        email: this.email,
+        username: this.username,
+        fullName: this.fullName
+    },
+        process.env.ACCESS_TOKEN_SECRET,
+        {
+            expiresIn: process.env.ACCESS_TOKEN_EXPIRY
+        })
+}
+
+userSchema.methods.generateRefreshToken = function () {
+    return jwt.sign({
+        _id: this._id,
+    },
+        process.env.REFRESH_TOKEN_SECRET,
+        {
+            expiresIn: process.env.REFRESH_TOKEN_EXPIRY
+        })
+}
+
+export const User = mongoose.model("User", userSchema);
--- src/utils/AsyncHandler.js
+++ src/utils/AsyncHandler.js
@@ -1,10 +1,10 @@
-const asyncHandler = (requestHandler) => {
+const AsyncHandler = (requestHandler) => {
     (req, res, next) => {
         Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err))
     }
 }
 
-export { asyncHandler };
+export { AsyncHandler };
 
 //   -----------Another Method ---------
 // const asyncHandler = (fn) => async (req, res, next) => {
