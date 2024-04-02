86a216689d6649be8c859db815e7ad280bce8482
Nishant Singh Yadav <75436573+yadavNishant2020@users.noreply.github.com>
2024-04-02 11:46:34 +0000

    req fix

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -1,6 +1,6 @@
 import { AsyncHandler } from "../utils/AsyncHandler.js";
 
-const registerUser = AsyncHandler(async (rew, res) => {
+const registerUser = AsyncHandler(async (req, res) => {
     res.status(200).json({
         message: "ok"
     })
